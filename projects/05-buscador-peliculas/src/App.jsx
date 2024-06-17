24f2b5ac78fa3eb2145a9697142f87e09592b8ae
Felipe Merchan <felipemerchanfuquen@gmail.com>
2024-06-17 00:07:45 -0500

    2. Fetching de datos
    Clase de useCallback, useMemo y useRef

--- projects/05-buscador-peliculas/src/App.jsx
+++ projects/05-buscador-peliculas/src/App.jsx
@@ -1,45 +1,7 @@
-import { useEffect, useState, useRef } from 'react';
-
 import './App.css'
 import { Movies } from './components/Movies'
 import { useMovies } from './hooks/useMovies';
-
-function useSearch() {
-  const [search, setSearch] = useState('');
-  const [error, setError] = useState(null);
-  /* isFirstInput que usa useRef es una bandera para saber
-  si es el primer render del formulario debido a que tenemos
-  una validación (search === '') que da true cuando el usuario
-  apenas ha llegado a la página loq ue ocasiona que se setee un error
-  (setError('No se puede buscar una película vacía')) cuando no es necesario. */
-  const isFirstInput = useRef(true);
-
-  useEffect(() => {
-    if (isFirstInput.current) {
-      isFirstInput.current = search === ''
-      return
-    }
-
-    if (search === '') {
-      setError('No se puede buscar una película vacía')
-      return
-    }
-
-    if (search.match(/^\d+$/)) {
-      setError('No se puede buscar una película con un número')
-      return
-    }
-
-    if (search.length < 3) {
-      setError('La búsqueda debe tener al menos 3 caracteres')
-      return
-    }
-
-    setError(null)
-  }, [search])
-
-  return { error, search, setSearch } 
-}
+import { useSearch } from './hooks/useSearch';
 
 function App() {
   /* useRef permite crear una referencia mutable que persiste
@@ -47,12 +9,12 @@ function App() {
   cualquier valor que podamos mutar como un identificador, un elemento
   del DOM, un contador, etc; y que cada vez que cambia no vuelve a renderizar
   el componente y esto es lo que hace useRef al useState. */
-  const { movies } = useMovies();
-  const { error, search, setSearch } = useSearch()
+  const { error, search, setSearch } = useSearch();
+  const { loading, movies, getMovies } = useMovies({ search });
 
   const handleSubmit = (event) => {
-    event.preventDefault()
-    console.log({ search })
+    event.preventDefault();
+    getMovies();
   }
 
   const handleChange = (event) => {
@@ -81,7 +43,9 @@ function App() {
         {error && <p style={{ color: 'red' }}>{error}</p>}
       </header>
       <main>
-        <Movies movies={movies} />
+        {
+          loading ? <p>Cargando...</p> : <Movies movies={movies} />
+        }
       </main>
     </div>
   )
--- projects/05-buscador-peliculas/src/hooks/useMovies.js
+++ projects/05-buscador-peliculas/src/hooks/useMovies.js
@@ -1,26 +1,30 @@
-import responseMovies from '../mocks/with-results.json'
-import withoutResults from '../mocks/no-results.json'
+import { useState } from 'react'
+
+import { searchMovies } from '../services/movies';
 
 /* Al tener nuestro custom hook nuestra aplicación
 es totalmente agnóstica de lo que está pasando dentro del
 custom hook. Por lo cual, podemos trabajar en el useMovies
 sin necesidad de tocar nuestra aplicación. Es decir, separamos
 las responsabilidades. */
-export function useMovies() {
-  const movies = responseMovies.Search
+export function useMovies({ search }) {
+  const [movies, setMovies] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null)
 
-  const mappedMovies = movies?.map(movie => ({
-    id: movie.imdbID,
-    image: movie.Poster,
-    title: movie.Title,
-    year: movie.Year,
-  }))
-
-  const getMovies = () => {
-    if (search) {
-
+  const getMovies = async () => {
+    try {
+      setLoading(true);
+      setError(null);
+      const newMovies = await searchMovies({ search });
+      setMovies(newMovies);
+    } catch (error) {
+      setError(error.message);
+    } finally {
+      /* Finally se ejecuta tanto después de el try o del catch */
+      setLoading(false);
     }
   }
 
-  return { movies: mappedMovies }
+  return { error, loading, movies, getMovies }
 }
