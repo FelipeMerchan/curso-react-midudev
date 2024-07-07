9b36c5299f8d19ff214ca5b19b7ae416d1c16eb9
Felipe Merchan <felipemerchanfuquen@gmail.com>
2024-07-06 21:02:15 -0500

    2. Fetching de datos

--- projects/04-react-prueba-tecnica/src/App.jsx
+++ projects/04-react-prueba-tecnica/src/App.jsx
@@ -1,5 +1,36 @@
+import { useEffect, useState } from "react";
+
+const catEndpointRandomFact = 'https://catfact.ninja/fact';
+//const catEndpointImageUrl = `https://cataas.com/cat/says/${firstWorld}?size=50&color=red&json=true`
+const catPrefixImageUrl = 'https://cataas.com'
+
 export function App() {
+  const [fact, setFact] = useState();
+  const [imageUrl, setImageUrl] = useState();
+
+  useEffect(() => {
+    fetch(catEndpointRandomFact)
+      .then(res => res.json())
+      .then(data => {
+        const { fact } = data;
+        setFact(fact);
+
+        const threeFirstWords = fact.split(' ', 3).join(' ');
+
+        fetch(`https://cataas.com/cat/says/${threeFirstWords}?size=50&color=red&json=true`)
+          .then(res => res.json())
+          .then(data => {
+            const { url } = data;
+            setImageUrl(url);
+          })
+      })
+  }, [])
+  
   return (
-    <h1>App</h1>
+    <main>
+      <h1>App</h1>
+      {fact && <p>{fact}</p>}
+      {imageUrl && <img src={`${catPrefixImageUrl}${imageUrl}`} alt={`Image extracted using the first three words for ${fact}`} />}
+    </main>
   )
 }
