3d9d671707d50c01d5bfb48a84adfe157f78a18f
Felipe Merchan <felipemerchanfuquen@gmail.com>
2024-07-28 13:54:00 -0500

    Crear useFilter hook

--- projects/06-shopping-cart/src/App.jsx
+++ projects/06-shopping-cart/src/App.jsx
@@ -3,9 +3,10 @@ import { Products } from './components/Products'
 import './index.css'
 import { products as initialProducts } from './mocks/products.json'
 import { Header } from './components/Header'
+import { Footer } from './components/Footer'
+import { IS_DEVELOPMENT } from './config'
 
-export const App = () => {
-  const [products, setProducts] = useState(initialProducts);
+function useFilters () {
   const [filters, setFilters] = useState({
     category: 'all',
     minPrice: 0,
@@ -20,12 +21,23 @@ export const App = () => {
     })
   }
 
+  return {
+    filters,
+    filterProducts,
+    setFilters,
+  }
+}
+
+export const App = () => {
+  const [products] = useState(initialProducts);
+  const { filters, filterProducts, setFilters } = useFilters()
   const filteredProducts = filterProducts(products)
 
   return (
     <>
       <Header changeFilters={setFilters} />
       <Products products={filteredProducts} />
+      {IS_DEVELOPMENT && <Footer />}
     </>
   )
 }
---
+++ projects/06-shopping-cart/src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import './Footer.css'
+
+export function Footer ({ filters }) {
+  // const { filters } = useFilters()
+
+  return (
+    <footer className='footer'>
+      {JSON.stringify(filters)}
+      <h4>Prueba técnica de React ⚛️ － <span>@midudev</span></h4>
+      <h5>Shopping Cart con useContext & useReducer</h5>
+    </footer>
+  )
+}
