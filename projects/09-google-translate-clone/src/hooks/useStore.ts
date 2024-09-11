825d7426af1778681e9ed0db2cd6e4e892020037
Felipe Merchan <felipemerchanfuquen@gmail.com>
2024-09-11 00:27:22 -0500

    2. Creando un custom hook

---
+++ projects/09-google-translate-clone/src/hooks/useStore.ts
@@ -0,0 +1,108 @@
+import { useReducer } from "react";
+
+import { type TranslatorAction, type TranslatorState } from "../types.d";
+
+const initialState: TranslatorState = {
+  fromLanguage: "auto",
+  fromText: "",
+  isLoading: false,
+  result: "",
+  toLanguage: "en",
+};
+
+function reducer(state: TranslatorState, action: TranslatorAction) {
+  const { type } = action;
+
+  if (type === "INTERCHANGE_LANGUAGES") {
+    return {
+      ...state,
+      fromLanguage: state.toLanguage,
+      toLanguage: state.fromLanguage,
+    };
+  }
+
+  if (type === "SET_FROM_LANGUAGE") {
+    return {
+      ...state,
+      fromLanguage: action.payload,
+    };
+  }
+
+  if (type === "SET_TO_LANGUAGE") {
+    return {
+      ...state,
+      toLanguage: action.payload,
+    };
+  }
+
+  if (type === "SET_FROM_TEXT") {
+    return {
+      ...state,
+      isLoading: true,
+      fromText: action.payload,
+      result: "",
+    };
+  }
+
+  if (type === "SET_RESULT") {
+    return {
+      ...state,
+      isLoading: false,
+      result: action.payload,
+    };
+  }
+
+  /* Un reducer siempre tiene que devovler un nuevo estado.
+  Si no es verdadera ningúna de las validaciones de los if
+  devolvemos el mismo estado: */
+  return state;
+}
+
+export function useStore() {
+  const [{ fromLanguage, fromText, isLoading, result, toLanguage }, dispatch] =
+  useReducer(reducer, initialState);
+
+  /* No debemos devolver en este custom hook directamente el dispatch,
+  en su lugar debemos crear las formas(funciones como interchangeLanguage) que tenemos de actualizar el estado.
+  Si retornaramos el dispatch y todos los componentes lo usan eso generaría
+  que estuvieramos atando todos los componentes a un contrato en concreto que es
+  utilizar el useReducer de React, si el día de mañana cambiamos a Redux o Zustand
+  no tienen por qué saberlo el resto de componentes. Es muy importante que el dispatch
+  no se vea en los componentes. Una mejor práctica es que dentro del custom hook exportemos
+  las funciones que cambian el estado y si el día de mañana cambiamos a Zustand el cambio
+  lo hacemos solo en el custom hook en estas funciones y no en todos los compoenntes:*/
+  const interchangeLanguage = () => {
+    dispatch({ type: 'INTERCHANGE_LANGUAGES' })
+  }
+
+  const setFromLanguage = (payload) => {
+    dispatch({ type: 'SET_FROM_LANGUAGE', payload })
+  }
+
+  const setToLanguage = (payload) => {
+    dispatch({ type: 'SET_TO_LANGUAGE', payload })
+  }
+
+  const setFromText = (payload) => {
+    dispatch({ type: 'SET_FROM_TEXT', payload })
+  }
+
+  const setResult = (payload) => {
+    dispatch({ type: 'SET_RESULT', payload })
+  }
+
+  return {
+    fromLanguage,
+    fromText,
+    isLoading,
+    result,
+    toLanguage,
+
+    /* Methods */
+    interchangeLanguage,
+    setFromLanguage,
+    setToLanguage,
+    setFromText,
+    setResult,
+  }
+}
