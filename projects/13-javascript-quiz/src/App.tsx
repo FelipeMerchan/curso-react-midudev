b4819c27605282cef536562d706f814bdcc248c0
Felipe Merchan <felipemerchanfuquen@gmail.com>
2024-10-08 22:38:53 -0500

    4. Estructura con Material UI(MUI)

--- projects/13-javascript-quiz/src/App.tsx
+++ projects/13-javascript-quiz/src/App.tsx
@@ -1,15 +1,24 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
+import { Container, Stack, Typography } from "@mui/material";
+import { JavaScriptLogo } from "./JavaScriptLogo";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <>
-      <h1>JavaScript Quizz</h1>
-    </>
+    <main>
+      <Container maxWidth="sm">
+        <Stack
+          direction="row"
+          gap={2}
+          alignItems="center"
+          justifyContent="center"
+        >
+          <JavaScriptLogo />
+          <Typography variant="h2" component="h1">
+            JavaScript Quizz
+          </Typography>
+        </Stack>
+      </Container>
+    </main>
   );
 }
 
