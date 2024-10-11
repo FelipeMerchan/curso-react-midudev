279beddcdbf67fb6cf48e841cc9e3a9c3c20e43d
Felipe Merchan <felipemerchanfuquen@gmail.com>
2024-10-10 23:43:30 -0500

    10. Agregando Syntax Highlighter

--- projects/13-javascript-quiz/src/Game.tsx
+++ projects/13-javascript-quiz/src/Game.tsx
@@ -1,11 +1,19 @@
 import { Card, IconButton, Stack, Typography } from "@mui/material";
 import { useQuestionsStore } from "./store/questions";
+import SyntaxReactHighlighter from "react-syntax-highlighter";
+import { gradientDark } from "react-syntax-highlighter/dist/esm/styles/hljs";
 import { type Question } from "./types";
 
 const Question = ({ info }: { info: Question }) => {
   return (
-    <Card variant="outlined">
+    <Card
+      variant="outlined"
+      sx={{ bgcolor: "#222", padding: 2, textAlign: "start" }}
+    >
       <Typography variant="h5">{info.question}</Typography>
+      <SyntaxReactHighlighter language="javascript" style={gradientDark}>
+        {info.code}
+      </SyntaxReactHighlighter>
     </Card>
   );
 };
