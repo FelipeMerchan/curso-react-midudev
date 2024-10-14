c84c04c26b9cb59888f8d614cb0ce2f311c05c55
Felipe Merchan <felipemerchanfuquen@gmail.com>
2024-10-13 23:51:28 -0500

    18. Leer la info del estado global

---
+++ projects/13-javascript-quiz/src/Footer.tsx
@@ -0,0 +1,22 @@
+import { useQuestionsStore } from "./store/questions";
+
+export const Footer = () => {
+  const questions = useQuestionsStore((state) => state.questions);
+
+  let correct = 0;
+  let incorrect = 0;
+  let unanswered = 0;
+
+  questions.forEach((question) => {
+    const { correctAnswer, userSelectedAnswer } = question;
+    if (userSelectedAnswer == null) unanswered++;
+    else if (userSelectedAnswer === correctAnswer) correct++;
+    else incorrect++;
+  });
+
+  return (
+    <footer style={{ marginTop: "16px" }}>
+      <strong>{`✔️ ${correct} correctas - ❌ ${incorrect} incorrectas - ❔ ${unanswered} sin responder`}</strong>
+    </footer>
+  );
+};
